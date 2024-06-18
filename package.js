29a5bd88350561002430e35d84bee5b6cd5b7114
Pierre Wieser <p.wieser@trychlos.org>
2024-06-18 09:13:00 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:ui-utils',
-    version: '1.1.0',
+    version: '1.1.1-rc',
     summary: 'UI utilities',
     git: 'https://github.com/trychlos/pwix:ui-utils',
     documentation: 'README.md'
